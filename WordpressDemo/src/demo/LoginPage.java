1cbbbf6839d2e3e4cdb2489aaa181b803364ac8c
Techfios2017 <30378494+Techfios2017@users.noreply.github.com>
2017-08-26 00:53:25 -0400

    added implicit wait and explicit wait

--- WordpressDemo/src/demo/LoginPage.java
+++ WordpressDemo/src/demo/LoginPage.java
@@ -1,9 +1,14 @@
 package demo;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class LoginPage {
 	
@@ -31,6 +36,14 @@ public class LoginPage {
 	
 	
 	public void login_demo(String uName, String pWord){
+		
+		//it will always wait for 10 seconds(implicit wait)
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		//wait until 10 seconds for the element to exist(explicit wait)
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+		 
+		WebElement element = wait.until(ExpectedConditions.elementToBeClickable(By.id("user_login")));
 		
 		username.sendKeys(uName);
 		password.sendKeys(pWord);
